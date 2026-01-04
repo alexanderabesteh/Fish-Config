1e0809d3c919a7b3eccb6ba7514861ad7d0ab67d
alexanderabesteh <alexander.abesteh@protonmail.com>
2026-01-04 17:17:24 -0500

    Update fastfetch function

--- functions/ff.fish
+++ functions/ff.fish
@@ -1,4 +1,6 @@
 function ff --wraps=fastfetch --description 'alias fastfetch'
-  
-  fastfetch --kitty-direct $HOME/Documents/Assets/Images/Haytham3.png
+
+  if not set -q NVIM_RUNNING
+    fastfetch --kitty-direct $HOME/Documents/Assets/Images/Haytham3.png
+  end
 end
